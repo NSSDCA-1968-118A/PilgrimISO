7c57eb04e79d0452f11d721aa6e8f1ad55a30124
furkan <115372686+NSSDCA-1968-118A@users.noreply.github.com>
2023-06-30 12:33:33 +0300

    first commit

---
+++ src/test/java/LoginRegistrationTests.java
@@ -0,0 +1,26 @@
+public class LoginRegistrationTests {
+
+	@Test
+	public void testLoginRegistrationWorksAsExpected() {
+		boolean loginRegistrationWorksAsExpected = true;
+		assertTrue(loginRegistrationWorksAsExpected);
+	}
+
+	@Test
+	public void firstTestCase() {
+		boolean isCorrect = true;
+		assertTrue(isCorrect);
+	}
+
+	@Test
+	public void secondTest() {
+		boolean doesItReallyWork = true;
+		assertTrue(doesWorkk);
+	}
+
+	public void anotherTest() {
+		boolean doesItReallyWork2 = true;
+		assertTrue(doesWorkkk);
+	}
+
+}
